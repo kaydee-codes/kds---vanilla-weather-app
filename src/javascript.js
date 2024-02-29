31d893308839f964b3feea82c411a97dc640f1e0
kaydee-codes <152921109+kaydee-codes@users.noreply.github.com>
2024-02-29 16:11:27 -0500

    added function to display current temperature based on search results

--- src/javascript.js
+++ src/javascript.js
@@ -1,8 +1,24 @@
+function refreshWeather(response) {
+  let tempElement = document.querySelector("#temp");
+  let temp = response.data.temperature.current;
+  let cityElement = document.querySelector("#app-city");
+
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = Math.round(temp);
+}
+
+function searchCity(city) {
+  let apiKey = "b408eca3f1b0e9991o40ab2t82d3f596";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#form-input");
   let cityElement = document.querySelector("#app-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
