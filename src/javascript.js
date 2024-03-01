fd9bc05fbf70f84e5e4af5f569e96a86f46033e0
kaydee-codes <152921109+kaydee-codes@users.noreply.github.com>
2024-03-01 14:10:01 -0500

    added description from API to html and javascript

--- src/javascript.js
+++ src/javascript.js
@@ -2,8 +2,10 @@ function refreshWeather(response) {
   let tempElement = document.querySelector("#temp");
   let temp = response.data.temperature.current;
   let cityElement = document.querySelector("#app-city");
+  let descriptionElement = document.querySelector("#description");
 
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
   tempElement.innerHTML = Math.round(temp);
 }
 
