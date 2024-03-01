d2f987c55da9652a441a1855a745306459d5b65c
kaydee-codes <152921109+kaydee-codes@users.noreply.github.com>
2024-03-01 14:44:12 -0500

    updated wind speed

--- src/javascript.js
+++ src/javascript.js
@@ -4,10 +4,12 @@ function refreshWeather(response) {
   let cityElement = document.querySelector("#app-city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
 
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement = `${response.data.wind.speed}km/h`;
   tempElement.innerHTML = Math.round(temp);
 }
 
